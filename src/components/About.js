65943b87e878b01f43b1fc23f946c6c4172ebd41
Lisa Harrison <lmharrison0810@gmail.com>
2022-07-18 17:37:24 +0100

    about section updated text and included zoom feature

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,78 @@
 import React from 'react';
-import Fade from "react-reveal";
+import { NavLink, Link } from 'react-router-dom';
+import { Fade, Zoom } from "react-reveal";
 import profilePic from "../assets/profilePic.jpg"
-import { Button, Container, Col, Row } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function About() {
     return (
-        <section id="about">
-            <Fade duration={1000}>
-                <Container>
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <img
-                                className="profile-pic"
-                                src={profilePic}
-                                alt="Lisa Profile Pic"
-                            />
-                            <div className="contact-details">
-                                <h2>Contact Me:</h2>
-                                <Button className="contact-btn" href="mailto:lmharrison0810@gmail.com" variant="custom-btn" size="sm" >
-                                    <FontAwesomeIcon icon={faEnvelope} size="xl" style={{ marginRight: "10px" }} /> Email
-                                </Button>
-                            </div>
-                        </Col>
-                        <Col className="about-me" xs={12} md={8}>
-                            <p>I’m a full-stack software engineer residing within the Northern Virginia Area.  Naturally inquisitive, I discovered coding and ventured down the rabbit hole and never looked back.  </p>
-                            <p> I love to get creative and enjoy nothing more than turning complex problems into simple, beautiful and intuitive designs. The ultimate goal is to build applications that users feel they simply cannot live without and never thought they needed. </p>
-                            <p>When i’m not pushing pixels and keyboard tapping, you’ll find me navigating the great outdoors hiking across mountains or motorcycling around them.</p>
+        <div className='about'>
+            <Row className='justify-content-between p-2 mt-lg-3'>
+                <Col md={4} className='text-center p-2'>
 
-                            <Button className="download" id="resume" href="" variant="custom" size="sm" >
-                                <FontAwesomeIcon icon={faArrowDown} size="xl" style={{ marginRight: "10px" }} /> Resume Download
-                            </Button>
-                        </Col>
-                    </Row>
-                </Container>
-            </Fade>
-        </section >
+                    {/* Image */}
+
+                    <Col className='profile-pic p-1'>
+                        <Zoom delay={750}>
+                            <img
+                                src={profilePic}
+                                alt='Lisa'
+                                className='img-fluid '
+                                style={{ borderRadius: '15%', border: '5px solid #e83283' }}
+                            />
+                        </Zoom>
+                    </Col>
+                    <Col className='my-2'>
+
+                        {/* Resume */}
+                       
+                        <div className='d-flex justify-content-center'>
+                            <div className='mx-1'>
+                                <Fade bottom delay={800}>
+                                    <Button className="download" id="resume" href="" variant="custom" size="sm" >
+                                        <FontAwesomeIcon icon={faArrowDown} size="xl" style={{ marginRight: "10px" }} /> Resume Download
+                                    </Button>
+                                </Fade>
+                            </div>
+                        </div>
+                    </Col>
+                </Col>
+
+                {/* ABOUT  */}
+                <Col md={8} className='mt-md-0 px-0 p-md-2'>
+                    <div className='m-2' >
+                        <Fade bottom delay={800}>
+                            <h2 className='about-intro'>Fullstack Developer.</h2>
+                        </Fade>
+                        <Fade bottom delay={800}>
+                            <p className='mb-2 text'>
+                            I’m a full-stack software engineer residing within the Northern Virginia Area.  Naturally inquisitive, I discovered coding and ventured down the rabbit hole and never looked back.
+                            </p>
+                        </Fade>
+                        <Fade bottom delay={800}>
+                            <p className='mb-2 text'>
+                                I have a Certificate in Fullstack Web and Mobile Development from <span className='text-primary'>
+                                    Nucamp Coding Bootcamp</span>, a Frontend Web Developer Certificate from <span className='text-primary'>George Washington University</span>,
+                                a Cloud Practitioner Certificate <span className='text-primary'>AWS</span> and a BA in Music <span className='text-primary'>Cardiff University, UK</span>.
+                            </p>
+                        </Fade>
+                        <Fade bottom delay={800}>
+                            <p className='mb-2 text'>
+                                Currently, I am seeking a position as a Junior Software Developer. Want to collaborate on a project or have an employment opportunity I would love to hear from you!
+                            </p>
+                        </Fade>
+                        <Fade bottom delay={800}>
+                            <NavLink tag='link' className='nav-link touch-link mx-0 p-0' to='/contact'>
+                                <u><em><h3>Contact me!</h3></em></u>
+                            </NavLink>
+                        </Fade>
+                    </div>
+                </Col>
+            </Row>
+            <hr className='mx-auto my-4 text-primary col-8' />
+        </div>
     );
 }
 
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,13 +6,13 @@ export const Intro = () => {
         <div >
             <div className='d-flex justify-content-center align-content-center' style={{maxWidth: '95%', height: '87vh', margin: 'auto'}}>
                 <div className='border-0 align-self-center p-auto'>
-                    <Fade left duration={3000} distance="300px">
+                    <Fade left duration={2000} distance="300px">
                         <h2 className='intro-text'>Lisa Harrison</h2>
                     </Fade>
-                    <Fade right duration={3000} distance="300px">
+                    <Fade right duration={2000} distance="300px">
                         <h2 className='mt-0 sub-text '>Junior Software Developer</h2>
                     </Fade>
-                    <Fade left duration={3000} distance="300px">
+                    <Fade left duration={2000} distance="300px">
                         <p className='mt-0 text'>
                         I love to get creative and build simple, beautiful and intuitive designs.<br/>
                         </p>
